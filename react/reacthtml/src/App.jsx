4fea31ad13a007706398ca8738074f4ecdc89058
Elvira Wiklund <wiklundel@gmail.com>
2025-03-18 12:36:38 +0100

    .

--- react/reacthtml/src/App.jsx
+++ react/reacthtml/src/App.jsx
@@ -1,18 +1,18 @@
-import Home from './pages/Home';
-import OmOss from './pages/OmOss';
-import Tavling from './pages/Tavling';
-import Kalender from './pages/Kalender';
-import Kontakt from './pages/Kontakt';
-import VaraHastar from './pages/VaraHastar';
-import BorjaRida from './pages/BorjaRida';
+import Home from "./pages/Index";
+import OmOss from "./pages/OmOss";
+import Tavling from "./pages/Tavling";
+import Kalender from "./pages/Kalender";
+import Kontakt from "./pages/Kontakt";
+import VaraHastar from "./pages/VaraHastar";
+import BorjaRida from "./pages/BorjaRida";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Sidemenu from './components/Sidemenu';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidemenu from "./components/Sidemenu";
 
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
